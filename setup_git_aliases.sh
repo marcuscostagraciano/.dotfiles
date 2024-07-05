ca9080968d2bf5a8d49984fe73a17b2b7a6576c8
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-07-04 22:22:49 -0300

    add new alias

--- setup_git_aliases.sh
+++ setup_git_aliases.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# --global all repos of the current user
+# --system all repos on the system (entire PC)
+# --local applied on the current repo
+
 git config --global alias.logdog 'log --decorate --oneline --graph --pretty="%C(#f4f910 bold)[%ad] | %C(#be257a no-bold)%h | %C(white bold)%s" --date=short'
+git config --global alias.llog 'log -1 --pretty=fuller'
